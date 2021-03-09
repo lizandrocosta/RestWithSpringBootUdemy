a8f934f0cabce833ae89d049da811fe875e01ff0
Lizandro Costa <lizandro.quinda@minfin.gov.ao>
2021-03-09 23:40:04 +0100

    Ola Spring Boot Udemy

---
+++ RestWithSpringBootUdemy_Eclipse/src/main/java/ao/inet/springbootudemy/startup.java
@@ -0,0 +1,15 @@
+package ao.inet.springbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Informar que a classe é uma aplicação spring boot
+
+@SpringBootApplication
+public class startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(startup.class, args);
+		
+	}
+}
